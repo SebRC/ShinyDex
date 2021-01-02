74de57b9d9550cf7e7d22a3516e0c862f47026a2
Sebastian Christiansen <seba4746@stud.kea.dk>
2021-01-02 14:56:13 +0100

    Rename folder and cleanup

--- ShinyDex/Utility/Extensions.swift
+++ ShinyDex/Utility/Extensions.swift
@@ -75,16 +75,6 @@ extension UIColor
 
 }
 
-extension UIButton
-{
-  func underline()
-{
-    let attributedString = NSMutableAttributedString(string: (self.titleLabel?.text!)!)
-	attributedString.addAttribute(NSAttributedString.Key.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: NSRange(location: 0, length: (self.titleLabel?.text!.count)!))
-    self.setAttributedTitle(attributedString, for: .normal)
-  }
-}
-
 extension UIView
 {
 	func initContentView(nibName: String, contentView: inout UIView?)
