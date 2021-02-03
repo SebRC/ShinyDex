f213db324346a9f1bcfb0b5919fa56250f500bfc
Sebastian Christiansen <seba4746@stud.kea.dk>
2021-02-03 16:45:39 +0100

    Replace Gifu with SDWebImageFLPlugin

--- ShinyDex/Utility/Extensions.swift
+++ ShinyDex/Utility/Extensions.swift
@@ -103,29 +103,3 @@ extension Double
 		return Double(truncating: decimalProbability * 100 as NSNumber)
 	}
 }
-
-extension UIImageView: GIFAnimatable {
-  private struct AssociatedKeys {
-    static var AnimatorKey = "gifu.animator.key"
-  }
-
-  override open func display(_ layer: CALayer) {
-    updateImageIfNeeded()
-  }
-
-  public var animator: Animator? {
-    get {
-      guard let animator = objc_getAssociatedObject(self, &AssociatedKeys.AnimatorKey) as? Animator else {
-        let animator = Animator(withDelegate: self)
-        self.animator = animator
-        return animator
-      }
-
-      return animator
-    }
-
-    set {
-      objc_setAssociatedObject(self, &AssociatedKeys.AnimatorKey, newValue as Animator?, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-    }
-  }
-}
