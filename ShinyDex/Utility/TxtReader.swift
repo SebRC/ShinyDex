6ac9f94b26537b35ecf8f1384d23beba6a5a8232
Sebastian Christiansen <seba4746@stud.kea.dk>
2021-05-20 18:20:26 +0200

    Add test for TxtReader

--- ShinyDex/Utility/TxtReader.swift
+++ ShinyDex/Utility/TxtReader.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public class TxtReader {
-	func linesFromResourceForced(textFile: String) -> [String] {
+	func readFile(textFile: String) -> [String] {
 		let path = Bundle.main.path(forResource: textFile, ofType: "txt")!
 		let content = try? String(contentsOfFile: path, encoding: String.Encoding.utf8)
 		var pokemonList = content!.components(separatedBy: "\n")
