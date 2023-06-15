14cb8993b222e9194f97238b64eea09cd7a056b3
Sebastian Refsbæk Christiansen <seb-chris@live.dk>
2023-06-15 10:19:45 +0200

    Tweak design of set encounters view

--- ShinyDex/UI/View/SetEncountersView.swift
+++ ShinyDex/UI/View/SetEncountersView.swift
@@ -5,14 +5,9 @@ import Foundation
 class SetEncountersView: UIView {
 	@IBOutlet weak var titleLabel: UILabel!
 	@IBOutlet weak var encountersTextField: UITextField!
-	@IBOutlet weak var nameLabel: UILabel!
-	@IBOutlet weak var numberLabel: UILabel!
-	@IBOutlet weak var encountersLabel: UILabel!
-	@IBOutlet weak var spriteImageView: UIImageView!
 	@IBOutlet weak var cancelButton: UIButton!
 	@IBOutlet weak var confirmButton: UIButton!
 	@IBOutlet weak var buttonSeparator: UIView!
-	@IBOutlet weak var spriteBackgroundView: UIView!
 
 	var colorService = ColorService()
 	
@@ -34,20 +29,11 @@ class SetEncountersView: UIView {
 	
 	fileprivate func setColors() {
 		contentView?.backgroundColor = colorService.getSecondaryColor()
-		spriteBackgroundView.backgroundColor = colorService.getPrimaryColor()
 		encountersTextField.backgroundColor = colorService.getPrimaryColor()
 		encountersTextField.textColor = colorService.getTertiaryColor()
-		encountersTextField.attributedPlaceholder = NSAttributedString(string: "Set Encounters",
-																	   attributes: [NSAttributedString.Key.foregroundColor: colorService.getTertiaryColor() as Any])
 		
 		titleLabel.backgroundColor = colorService.getPrimaryColor()
 		titleLabel.textColor = colorService.getTertiaryColor()
-		nameLabel.backgroundColor = colorService.getPrimaryColor()
-		nameLabel.textColor = colorService.getTertiaryColor()
-		numberLabel.backgroundColor = colorService.getPrimaryColor()
-		numberLabel.textColor = colorService.getTertiaryColor()
-		encountersLabel.backgroundColor = colorService.getPrimaryColor()
-		encountersLabel.textColor = colorService.getTertiaryColor()
 		cancelButton.backgroundColor = colorService.getPrimaryColor()
 		cancelButton.setTitleColor(colorService.getTertiaryColor(), for: .normal)
 		confirmButton.backgroundColor = colorService.getPrimaryColor()
@@ -56,10 +42,6 @@ class SetEncountersView: UIView {
 	}
 	
 	fileprivate func roundCorners() {
-		nameLabel.layer.cornerRadius = CornerRadius.standard
-		numberLabel.layer.cornerRadius = CornerRadius.standard
-		encountersLabel.layer.cornerRadius = CornerRadius.standard
-		spriteBackgroundView.layer.cornerRadius = CornerRadius.standard
 		layer.cornerRadius = CornerRadius.standard
 	}
 }
